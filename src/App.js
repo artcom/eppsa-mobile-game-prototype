7c32fdde70ca829dd5d32ffdbb37c1fe8759d8af
Jens Eraßmy <jens.erassmy@artcom.de>
2017-08-22 14:27:20 +0200

    Implement simple qr-reader component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React from "react"
-import logo from "./logo.svg"
+import styled, { injectGlobal } from "styled-components"
 import "./App.css"
+import QrReader from "react-qr-reader"
 
-export default function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  )
+// eslint-disable-next-line no-unused-expressions
+injectGlobal`
+  #root, html, body {
+    height: 100%;
+  }
+  
+  body {
+	margin: 0;
+  }
+`
+
+const Container = styled.div`
+`
+
+const QrReaderComponent = styled(QrReader)`
+  display: flex;
+  justify-content: center;
+`
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = { result: null }
+  }
+
+  render() {
+    return (
+      <Container>
+        <QrReaderComponent
+          delay={ 100 }
+          onError={ (error) => console.log(error) }
+          onScan={ (result) => this.setState({ result }) }
+          style={ { height: "100vw" } } />
+        { this.state.result && <div>{ this.state.result }</div> }
+      </Container>
+    )
+  }
 }

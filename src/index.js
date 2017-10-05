86a0af1fcf5690742cfc6adf4dec0e653c061c7b
Jens Eraßmy <jens.erassmy@artcom.de>
2017-10-05 16:37:02 +0200

    Connect with server over HTTPS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-unused-vars,import/no-extraneous-dependencies */
 import React from "react"
 import ReactDOM from "react-dom"
+import client from "socket.io-client"
+
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
 
 ReactDOM.render(<App />, document.getElementById("root"))
 registerServiceWorker()
+
+const socket = client("https://localhost:5000", { secure: true })
--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,32 @@
 /* eslint-disable import/no-commonjs */
 
 const express = require("express")
-const http = require("http")
+const fs = require("fs")
+const https = require("https")
+const socketIO = require("socket.io")
+
+const options = {
+  key: fs.readFileSync(`${__dirname}/file.pem`, "utf8"),
+  cert: fs.readFileSync(`${__dirname}/file.crt`, "utf8")
+}
 
 const app = express()
-const server = http.Server(app)
+const server = https.createServer(options, app)
+const socket = socketIO(server)
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
+socket.on("connection", () => {
+  console.log("A user connected.")
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected")
+  })
+})
+
+
 server.listen(5000, () => {
-  console.log("Example appstening on port 5000!")
+  console.log("Example listening on port 5000!")
 })

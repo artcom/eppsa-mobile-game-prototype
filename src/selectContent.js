6019f8ec17335ed93d7c9a206d389938c28b9424
Marco <marco.mojica@artcom.de>
2018-01-16 17:19:20 +0100

    Get data from cms
    data is imported like it was requested from cms

--- src/selectContent.js
+++ src/selectContent.js
@@ -1,15 +1,24 @@
 import memoize from "lodash/memoize"
 import mapValues from "lodash/mapValues"
+import omit from "lodash/omit"
+import keys from "lodash/keys"
 
-import data from "./data.json"
+import cmsData from "./cmsData.json"
+
+const data = {}
+data.game = transform(cmsData)
 
 export const selectPlayerContent = memoize((playerId) => {
-  const player = data.game.players[playerId]
-  const playerQuestIds = data.game.players[playerId].questIds
-  const playerQuests = playerQuestIds.reduce((obj, questId) => ({
-    ...obj,
-    [questId]: data.game.quests[questId]
-  }), {})
+  const player = data.game.players[`player-${playerId + 1}`]
+  const playerQuestIds = player.quests.split(",").map(val => val.trim())
+  const playerQuests = playerQuestIds.reduce((obj, questId) => {
+    const out = {
+      ...obj,
+      [questId]: data.game.quests[questId]
+    }
+    out[questId].items = out[questId].items.split(",").map(val => val.trim())
+    return out
+  }, {})
   const playerQuestItemIds = playerQuestIds
     .map(questId => data.game.quests[questId].items)
     .reduce((a, b) => a.concat(b), [])
@@ -23,18 +32,20 @@ export const selectPlayerContent = memoize((playerId) => {
 })
 
 export const selectSharedContent = memoize(() => {
-  const items = mapValues(data.game.items, (item, itemId) => (
-    {
-      name: item.name,
-      description: item.description,
-      questId: getQuestId(itemId)
-    }
-  ))
+  if (data) {
+    const items = mapValues(data.game.items, (item, itemId) => (
+      {
+        name: item.name,
+        description: item.description,
+        questId: getQuestId(itemId)
+      }
+    ))
 
-  return {
-    name: data.game.name,
-    description: data.game.description,
-    items
+    return {
+      name: data.game.name,
+      description: data.game.description,
+      items
+    }
   }
 })
 
@@ -44,4 +55,12 @@ function getQuestId(itemId) {
   )
 
   return questId
+}
+
+export function transform(content) {
+  const result = omit(content.index, "template")
+  keys(omit(content, "index")).forEach(key => {
+    result[key] = transform(content[key])
+  })
+  return result
 }

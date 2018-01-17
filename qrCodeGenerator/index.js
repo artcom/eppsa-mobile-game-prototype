3d887a6c20b9745872a4209aa5788458045471fb
Marco <marco.mojica@artcom.de>
2018-01-17 17:39:57 +0100

    Use fixed width for qr codes

--- qrCodeGenerator/index.js
+++ qrCodeGenerator/index.js
@@ -3,7 +3,9 @@ const QRCode = require('qrcode')
 
 Object.keys(cmsData.items).forEach(key => {
   console.log(key)
-  QRCode.toFile(`qrCodes/${key}.png`, `https://10.1.34.110:3000/${key}`, err => {
+  QRCode.toFile(`qrCodes/${key}.png`, `https://10.1.34.110:3000/${key}`,{
+    "width": 250
+  }, err => {
     if (err) throw  err
     console.log("done")
   })

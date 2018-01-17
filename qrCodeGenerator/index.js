68d134a42f632adbfa1812bfe393d821bb04d1d0
Marco <marco.mojica@artcom.de>
2018-01-17 17:36:22 +0100

    Add qrCode generator

---
+++ qrCodeGenerator/index.js
@@ -0,0 +1,10 @@
+const cmsData = require("../client/src/cmsData.json")
+const QRCode = require('qrcode')
+
+Object.keys(cmsData.items).forEach(key => {
+  console.log(key)
+  QRCode.toFile(`qrCodes/${key}.png`, `https://10.1.34.110:3000/${key}`, err => {
+    if (err) throw  err
+    console.log("done")
+  })
+})

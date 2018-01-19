cf521963a06c6d1ccd9f91bc7b44ae6de741cead
Marco <marco.mojica@artcom.de>
2018-01-19 11:06:00 +0100

    load items for qr code generation from cms
    create qrCodes dir when it does not exist

--- qrCodeGenerator/index.js
+++ qrCodeGenerator/index.js
@@ -1,12 +1,39 @@
-const cmsData = require("../client/src/cmsData.json")
+const fs = require("fs")
+const https = require("https")
 const QRCode = require('qrcode')
 
-Object.keys(cmsData.items).forEach(key => {
-  console.log(key)
-  QRCode.toFile(`qrCodes/${key}.png`, `https://10.1.34.110:3000/${key}`,{
-    "width": 250
-  }, err => {
-    if (err) throw  err
-    console.log("done")
+fs.access("qrCodes", err => {
+  if (err) {
+    fs.mkdir("qrCodes", err => {
+      if (err) throw err
+    })
+  }
+})
+
+https.get({
+  "hostname": "localhost",
+  "path": "/content/master/content/items",
+  "rejectUnauthorized": true
+}, res => {
+
+  res.setEncoding("utf8")
+
+  let rawData = ""
+
+  res.on("data", chunk => {
+    rawData += chunk
+  })
+
+  res.on("end", () => {
+    const items = JSON.parse(rawData)
+    Object.keys(items).forEach(key => {
+      console.log(key)
+      QRCode.toFile(`qrCodes/${key}.png`, `https://10.1.34.110:3000/${key}`,{
+        "width": 250
+      }, err => {
+        if (err) throw  err
+        console.log("done")
+      })
+    })
   })
 })
